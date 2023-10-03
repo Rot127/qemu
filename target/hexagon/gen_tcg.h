bf8368cd5af5d03799cea2ac327dd5be899cdea7
Rot127 <unisono@quyllur.org>
2023-10-03 12:32:20 -0500

    Fix logging of endframe.

--- target/hexagon/gen_tcg.h
+++ target/hexagon/gen_tcg.h
@@ -1367,7 +1367,7 @@
         uiV = uiV; \
         tcg_gen_movi_tl(hex_gpr[HEX_REG_PC], ctx->pkt->pc); \
         TCGv excp = tcg_constant_tl(HEX_EXCP_TRAP0); \
-        gen_helper_raise_exception(cpu_env, excp); \
         gen_helper_trace_endframe(cpu_env, hex_gpr[HEX_REG_PC], tcg_constant_i32(1)); \
+        gen_helper_raise_exception(cpu_env, excp); \
     } while (0)
 #endif
